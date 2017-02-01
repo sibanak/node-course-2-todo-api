5fa678a86c08c9a74978f28fad163cb8597d3c00
Stéphane Bernais <stephane.bernais@gmail.com>
2017-02-01 13:37:53 -0500

    add GET /todos route

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ var app = express();
 
 app.use(bodyParser.json());
 
+// POST
 app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
@@ -16,6 +17,15 @@ app.post('/todos', (req, res) => {
 
   todo.save().then((doc) => {
     res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+// GET
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
   }, (e) => {
     res.status(400).send(e);
   });
